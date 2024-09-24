f7d7c7f5f6bc2e279bcdd92a4876d32f99a1ea91
Cetadrius <vcetadrius@gmail.com>
2024-09-24 09:50:13 +0100

    Add files via upload

---
+++ C/C08/ex01/ft_boolean.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afilipe- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 14:41:26 by afilipe-          #+#    #+#             */
+/*   Updated: 2024/09/23 14:41:49 by afilipe-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+
+static inline int	is_even(int nb)
+{
+	return (nb % 2 == 0);
+}
+
+# define EVEN(value) is_even(value)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+# define TRUE 1
+# define FALSE 0
+
+typedef int	t_bool;
+
+#endif
